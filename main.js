85849e35d0bfc5f3d29e00eaf8660dec99ecc466
NicolasSeraphim <nicolas.s.serafim@gmail.com>
2024-10-14 14:56:55 -0300

    Jogo forca - Finalizado

--- main.js
+++ main.js
@@ -27,9 +27,155 @@ function iniciarJogo() {
     }
     document.querySelector('h2').innerHTML = palavraOculta;
     document.getElementById('btnReiniciar').classList.add('d-none');
+    desenharForca();
+
 }
 
 function checarLetra(letra) {
-    console.log(letra)
+   // console.log(letra);
+   if (!jogando) return;
+   let btn = document.getElementById('btn-'+letra);
+   let achou = false;
+
+   for (let i = 0; i < palavraSecreta.length; i++){
+        if (palavraSecreta[i] == letra.toLowerCase()){
+            achou = true;
+            palavraOculta = trocaLetra(palavraOculta, letra, i)
+        }
+
+    }
+    document.querySelector('h2').innerHTML = palavraOculta
+    btn.classList.remove('btn-outline-dark');
+    btn.classList.add(achou ? 'btn-primary' : 'btn-danger');
+    if(!achou) {
+        tentativasUsadas += 1;
+        desenharForca();    
+    }
+    checarJogo();
+}
+
+function checarJogo() {
+    if (tentativasUsadas == 6) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Ooops...',
+            text: 'Você Perdeu!!!'
+        });
+        jogando = false;
+        document.getElementById('btnReiniciar').classList.remove('d-none');
+    }
+    let listaTexto = palavraOculta.split(' ');
+    let novaPalavra = listaTexto.join('');
+    if (novaPalavra.toLowerCase() == palavraSecreta) 
+    {
+        Swal.fire ({
+            icon: "success",
+            title: "Aeeeeeee",
+            text: "Você ganhou!!!!"
+        });
+        jogando = false;
+        document.getElementById('btnReiniciar').classList.remove('d-none');        
+    }
+}
+
+function trocaLetra(textoOriginal, letra, posicao) {
+    let listaTexto = textoOriginal.split(' ');
+    listaTexto[posicao] = letra;
+    return listaTexto.join(' ');
+}
+
+function desenharForca(){
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = 6;
+    
+    // base da forca
+    ctx.beginPath();
+    ctx.strokeStyle = '#006700';
+    ctx.moveTo(20, canvas.height-10);
+    ctx.lineTo(180, canvas.height-10);
+    ctx.stroke();
+
+    // poste
+    ctx.beginPath();
+    ctx.strokeStyle = '#4e2708';
+    ctx.moveTo(60, canvas.height-10);
+    ctx.lineTo(60, 20);
+    ctx.stroke();
+
+    // trave
+    ctx.beginPath();
+    ctx.strokeStyle = '#4e2700';
+    ctx.moveTo(57, 20);
+    ctx.lineTo(120, 20);
+    ctx.stroke();
+
+    // corda
+    ctx.beginPath();
+    ctx.strokeStyle = '#4e2700';
+    ctx.moveTo(120, 17);
+    ctx.lineTo(120, 30);
+    ctx.stroke();
+
+    //trave diagonal
+    ctx.beginPath();
+    ctx.strokeStyle = '#4e2700';
+    ctx.moveTo(80, 20);
+    ctx.lineTo(60, 40);
+    ctx.stroke();
+
+    ctx.strokeStyle = '#000';
+    ctx.lineWidth = 2;
+
+    
+    // cabeça
+    if (tentativasUsadas >= 1 ) {
+        ctx.beginPath();
+        ctx.arc(120, 45, 15, 0, Math.PI * 2);
+        ctx.stroke();
+    }
+
+    //corpo
+    if (tentativasUsadas >= 2) {
+        ctx.beginPath();
+        ctx.moveTo(120, 60);
+        ctx.lineTo(120, 120);
+        ctx.stroke();
+    }
+
+    // braço esquerdo
+    if (tentativasUsadas >= 3) {
+        ctx.beginPath();
+        ctx.moveTo(120, 70);
+        ctx.lineTo(100, 100);
+        ctx.stroke();
+    }
+
+    //braço direito
+    if (tentativasUsadas >= 4) {
+        ctx.beginPath();
+        ctx.moveTo(120, 70);
+        ctx.lineTo(140, 100);
+        ctx.stroke();
+    }
+
+    //perna esquerda
+    if (tentativasUsadas >= 5) {
+        ctx.beginPath();
+        ctx.moveTo(120, 120);
+        ctx.lineTo(100, 150);
+        ctx.stroke();
+    }
+
+    //perna direita
+    if (tentativasUsadas >= 6) {
+        ctx.beginPath();
+        ctx.moveTo(120, 120);
+        ctx.lineTo(140, 150);
+        ctx.stroke();
+    }
+
 }
 
